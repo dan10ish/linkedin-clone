455e3519b787f3a461d85557c8ca36348eaa6c6d
Danish Ansari <aaansaridan@gmail.com>
2021-08-08 01:08:02 +0530

    Added Feed Posts

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from './InputOption';
@@ -6,8 +6,16 @@ import ImageIcon from "@material-ui/icons/Image";
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from './Post';
 
 function Feed() {
+
+    const [posts, setPosts] = useState([]);
+
+    const sendPost = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
@@ -15,7 +23,7 @@ function Feed() {
                     <CreateIcon />
                     <form>
                         <input type="text" />
-                        <button type='submit'>Send</button>
+                        <button onClick={sendPost} type='submit'>Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -25,6 +33,15 @@ function Feed() {
                     <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
                 </div>
             </div>
+
+            {/* Posts */}
+            {posts.map((post) => (
+                <Post />
+            ))}
+
+
+            <Post name='Danish Ansari' description='This is a test' message='Wow this worked!' />
+
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHVAesGWNVKk5G0PAX3FXWhbGuRucbhAc",
+    authDomain: "linkedin-clone-667e9.firebaseapp.com",
+    projectId: "linkedin-clone-667e9",
+    storageBucket: "linkedin-clone-667e9.appspot.com",
+    messagingSenderId: "752390658933",
+    appId: "1:752390658933:web:c59af2f2cb56982f40513c"
+};
