b3dadd12771ee449317e9e29dbee423f174bab58
Danish Ansari <aaansaridan@gmail.com>
2021-08-07 13:14:16 +0530

    Added Header

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import './Header.css'
+import linkedinLogo from './Resources/linkedin.svg';
+import HeaderOption from './HeaderOption';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
         <div className='header'>
-            <h2>This is the header</h2>
-
             <div className="header__left">
-                <img src="" alt="" />
+                <img src={linkedinLogo} alt="" />
 
                 <div className="header__search">
-                    {/* SearchIcon */}
+                    <SearchIcon />
                     <input type="text" />
                 </div>
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="https://avatars.githubusercontent.com/u/63841089?v=4" title="Me" />
             </div>
         </div>
     )
